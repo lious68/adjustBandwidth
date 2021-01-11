be6f0bc50697c45c371cebdb6f9ebc8ac85a96cb
bright <lious68@163.com>
2021-01-11 16:55:20 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,29 @@
+#-*- encoding: utf-8 -*-
+#配置公私钥"""
+public_key = '6LGIf66kV44q566d7yo9so1Qc+kyzvRCueDmL14zYNTAdDRhOAZ52YtjRsNubcKQ/2AIkw'			#账户公私钥中的公钥
+private_key = 'Xn2Oj+R34jsMVd6YF8R+Q33bGUvOHlwhp80Qi/+eZVr81vfJZsIp+wVMg/RjbzuGSwwie'		#账户公私钥中的私钥
+project_id = "org-0pj1ydt" # 项目ID 请在Dashbord 上获取
+base_url    = "https://api.ucloud.cn"
+
+run_mode = 'manual' #(auto|manual|alarm)
+#auto 自动获取项目中所有EIP
+#manual 指定EIP方式，指定EIP，放到eipIdArray[]里。
+#alarm 通过告警出来的EIP，自动获取
+eipIdArray = ['eip-xdvctmxb'] #人工填写需要自动伸缩的EIP，仅在manual模式下有效。
+
+adjust_method = 'static' #(dynamic|package|static) #对应三种带宽调整方法，动态调整、带宽包和静态方法
+
+#if dynamic
+percent = 0.5 #自动调整带宽的步长，百分比。
+#if package， parameters
+package_size = 10  #带宽包大小
+time_range = 1 #持续时间，1小时
+#if static
+stepBandwidth = 1 #每次扩容/缩容的带宽大小，单位M
+
+#全局通用参数
+maxBandwidth = 10  #EIP的最大带宽
+minBandwidth = 1   #EIP的最小带宽
+noAdjustEip = ['eip-xdvctmx','eip-y3kify'] #三种模式下都有效。手动填写，不参与调整的EIPid
+durtime = 30 #下次调整带宽时间间隔，单位秒
+region = 'hk' #可用区，请参考region文件
